1c8767f881019d7e3a685340bd88795271ea5c9d
Hamza Mushtaq <hamza@twinmatrix.net>
2026-04-24 18:35:20 +0500

    switch back to sandbox.

--- src/features/dashboard/DashboardLayout.tsx
+++ src/features/dashboard/DashboardLayout.tsx
@@ -364,7 +364,6 @@ const DashboardLayoutContent: React.FC = () => {
           accessToken={appConfig.metaAtlas?.accessToken || ''}
           secretKey={appConfig.metaAtlas?.secretKey || ''}
           role={appConfig.metaAtlas?.role || ''}
-          environment='agil'
         >
           {/* MapImageLoader loads custom icons/images for use in map layers */}
           <MapImageLoader />
